a531352399232d861de30d1dbf1467e01ab24322
alexandramcadam <107989107+alexandramcadam@users.noreply.github.com>
2024-02-29 13:13:53 +0000

    minor changes and testing comments

--- gates.py
+++ gates.py
@@ -1,40 +1,57 @@
 import numpy as np
 
-class gates(object):
-  def __init__(self, qubit):
-    self.qubit = qubit
-    
+
+class Gates(object):
+
+  def __init__(self, n):
+    self.n = n
+    self.state = np.zeros(2**self.n, dtype=complex)
+    self.state[0] = 1  #not sure why this is set to 1?
+
+  # Apply Harmard gate to given qubit
   def apply_hadamard(self, qubit):
     H = np.array([[1, 1], [1, -1]])
     x = 1 / np.sqrt(2)
     hada = np.multiply(H, x)
     hadamard = np.multiply(hada, qubit)
     return hadamard
-    
+
+  # Apply phase shift to given qubit
   def apply_phase_shift(self, phi, qubit):
-    phase = np.array([1, 0],[0, np.exp(1j*phi)])
+    phase = np.array([1, 0], [0, np.exp(1j * phi)])
     phase_shift = np.multiply(phase, qubit)
     return phase_shift
 
+  # Apply CNOT to given qubit
   def apply_cnot(self, control, target):
-    if control[0] == 0 and control[1] == 1:
-      target[0] = target[1]
-      target[1] = target[0]  # flipping the target qubit
-    elif control[0] == 1 and control[1] == 0:
-      pass  # do nothing
-    else:
-      pass  # do nothing
-    return control, target
+    target = control  # target become copy of control
+    return control, target  #  return both target and control
+
+  # Apply controlled_v to given qubit
   def apply_controlled_v(self, qubit):
     V = np.array([[1, 0], [0, 1j]])
     controlled_v = np.multiply(V, qubit)
     return controlled_v
-  def and_gate(self, qubit):
-    
-    
-    
-    
-    
-      
-    
-    
+
+  # Not_gate takes one state to the other
+  def not_gate(self, qubit):
+    not_gate = np.array([[0, 1], [1, 0]])
+    not_qubit = np.multiply(not_gate, qubit)
+    return not_qubit
+
+  # EPR pair
+  def epr_pair(self, qubit1, qubit2):
+    H_qubit = self.apply_hadamard(qubit1)
+    control, target = self.apply_cnot(H_qubit, qubit2)
+    '''insert this line qunatum state consists of control and target'''
+    return state
+
+  #print method - quickly see current state without measurement
+  def __str__(self):
+    return (
+        f'There are {self.n} qubits, the state of the register is {self.state}'
+    )
+
+  #def apply_gate(self, ): #reduced repetition in gate functions
+
+  #def measure(self):
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-#main is for calling simulator, algorithms etc.
+#main is for calling simulator and implementing the algorithms and visualisation (if any)
 #need to import other files so can call classes etc.
 #use for testing
 
-import time
-import simulator
+from gates import Gates
 
+#testing
 
+psi = Gates(2)  #initialise 2 qubit register
+print(psi)  #print initial state
 
+h = psi.apply_hadamard(
+    psi.state[0])  #psi.state[0] is qubit 0, can make simpler
+print(psi)
+print(h)
+#NOTE should update so psi=h when gate applied, easier to apply next gate
+hh = psi.apply_hadamard(psi.state[0]).apply_hadamard(psi.state[0])
+#applying twice doesnt work since apply_hadamard returns an array not the object
 
-
+#Grover's algorithm
+#Printing and visualisations
